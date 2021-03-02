c443fc69f0e6382205b88cdeaeff883fb7556f37
Nabeel Ahsen <nabeel.ahsen@shopify.com>
2021-03-02 09:08:27 -0500

    Create Shop model

--- sample_django_app/shopify_app/models.py
+++ sample_django_app/shopify_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Shop(models.Model):
+    shopify_domain = models.CharField(max_length=255)
+    shopify_token = models.CharField(max_length=255)
+    access_scopes = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "id: %d, shop: %s, token: %s, access_scopes: %s" % (self.pk, self.shopify_domain, self.shopify_token, self.access_scopes)
