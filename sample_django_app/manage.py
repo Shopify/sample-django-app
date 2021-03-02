fd2edbb9b9da62e6a3f9184015ab1aec66074fb8
Nabeel Ahsen <nabeel.ahsen@shopify.com>
2021-03-01 21:22:23 -0500

    Rename project to sample_django_app

--- sample_django_app/manage.py
+++ sample_django_app/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shopify_app.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sample_django_app.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- sample_django_app/sample_django_app/wsgi.py
+++ sample_django_app/sample_django_app/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for shopify_app project.
+WSGI config for sample_django_app project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shopify_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sample_django_app.settings')
 
 application = get_wsgi_application()
